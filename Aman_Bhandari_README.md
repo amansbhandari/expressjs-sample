845b8faba39b64be3fc046e73811432066fabf72
Aman Singh Bhandari <am727005@dal.ca>
2022-06-25 14:23:23 -0300

    final commit

---
+++ Aman_Bhandari_README.md
@@ -0,0 +1,29 @@
+## Heruko Deployed URl
+
+https://csci5709-tutorial4-aman.herokuapp.com/
+
+## GitLab Repository URl
+
+https://git.cs.dal.ca/bhandari/csci5709-assignments/-/tree/tutorial5
+Repository Name : csci5709-assignments
+Branch: tutorial5
+
+## Credit to
+
+https://stackabuse.com/reading-and-writing-json-files-with-node-js/
+
+The above article helped me to read and write to JSON files to add and update the users from the list. I copied the code from this URL and edit it to replace it my dynamic editing and addition of user data.
+
+https://expressjs.com/en/guide/routing.html
+
+The article above helped me to learn how to get the route paramters, body of PUT and POST. I didn't copy the code rather i learned and tried same at my end.
+
+https://www.codegrepper.com/code-examples/javascript/nodejs+get+current+timestamp
+
+The article above helped me to know how to get the current datetimestamp in javascript. I used timestamp to create a new Id on call of Post.
+
+## Student details
+
+Name : Aman Singh Bhandari
+BannerId : B00910008
+Email Id : am727005@dal.ca
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,130 @@
+("use strict");
+
+const { throws } = require("assert");
 const express = require("express");
+
+const fs = require("fs");
+
+const dataPath = "./data/user.json";
 
 const router = express.Router();
 
-// const users = require("../data/users");
-
 router.get("/users", (req, res) => {
   try {
+    let rawdata = fs.readFileSync(dataPath);
+    let users = JSON.parse(rawdata);
+
     return res.status(200).json({
-      message: "user retrived",
+      message: "Users retrived",
+      success: true,
+      users: users,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+});
+
+router.put("/update/:id", (req, res) => {
+  try {
+    let rawdata = fs.readFileSync(dataPath);
+    let users = JSON.parse(rawdata);
+    let found = 0;
+
+    for (let i = 0; i < users.length; i++) {
+      if (req.params.id === users[i].id) {
+        users[i].firstName = req.body.firstName;
+        users[i].email = req.body.email;
+        found = 1;
+        break;
+      }
+    }
+
+    if (found === 1) {
+      //found
+      let data = JSON.stringify(users);
+      fs.writeFileSync(dataPath, data);
+      return res.status(200).json({
+        message: "User updated",
+        success: true,
+      });
+    } else {
+      return res.status(400).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+});
+
+router.post("/add", (req, res) => {
+  try {
+    let rawdata = fs.readFileSync(dataPath);
+    let users = JSON.parse(rawdata);
+
+    for (let i = 0; i < users.length; i++) {
+      if (req.body.email === users[i].email) {
+        return res.status(400).json({
+          message: "User already exists",
+          success: false,
+        });
+      }
+    }
+
+    const id = Date.now(); //always new
+    users[users.length] = {
+      id: id + "",
+      email: req.body.email,
+      firstName: req.body.firstName,
+    };
+    let data = JSON.stringify(users);
+    fs.writeFileSync(dataPath, data);
+    return res.status(200).json({
+      message: "User added",
       success: true,
     });
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+});
+
+router.get("/user/:id", (req, res) => {
+  try {
+    let rawdata = fs.readFileSync(dataPath);
+    let users = JSON.parse(rawdata);
+
+    for (let i = 0; i < users.length; i++) {
+      if (req.params.id === users[i].id) {
+        return res.status(200).json({
+          user: users[i],
+          success: true,
+        });
+      }
+    }
+
+    return res.status(400).json({
+      message: "User not found",
+      success: false,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
 });
 
 module.exports = router;
